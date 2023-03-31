d7cf4af71f6d8616afb05ec646d80a05a33e5a30
G'anisher <cyberredweb@gmail.com>
2023-04-01 00:40:26 +0500

    modified:   scraper.py

--- scraper.py
+++ scraper.py
@@ -2,79 +2,116 @@ import requests
 from bs4 import BeautifulSoup
 from db import db
 import json
+import pprint
+
 # bazani borligini tekshirib olamiz
 db.initialize()
 # institut manzili
 URL = 'https://student.andmiedu.uz'
-# bu yerda telegram botni ishlatmoqchi bo'lgan talaba login va parolini beradi, va shu login parol oraqli saytga kirib u yerda biz cookielarni ovolamiz, agar login yoki parol xato bo'lsa, funksiya False return qiladi
-# Bu faqatgina bir marta olinib saqlab qo'yilishi kerak, har safar ishlatsek kirishlar tarixida ko'payib ketadi, bizi kirganimiz
-def auth(login, password):
-    req = requests.get(URL + '/dashboard/login')
-    req_html = req.text
-    soup = BeautifulSoup(req_html,'lxml')
-    hidden_input = soup.input.get('value')
+# strukturasini o'zgartiryapman, ba'zi commentlar to'g'ri bo'lmasligi mumkin
+class SCRAPER():
+    def __init__(self, login, password):
+        self.login = login
+        self.password = password
+        # shu asosiy cookielarni olib beruvchi funksiya bo'lib ishlashligi kerak
+        
+        # bu yerda telegram botni ishlatmoqchi bo'lgan talaba login va parolini beradi, va shu login parol oraqli saytga kirib u yerda biz cookielarni ovolamiz, agar login yoki parol xato bo'lsa, funksiya False return qiladi
+        # Bu faqatgina bir marta olinib saqlab qo'yilishi kerak, har safar ishlatsek kirishlar tarixida ko'payib ketadi, bizi kirganimiz
+        def auth(login, password):
+            req = requests.get(URL + '/dashboard/login')
+            req_html = req.text
+            soup = BeautifulSoup(req_html,'lxml')
+            hidden_input = soup.input.get('value')
 
-    payload = {
-        'FormStudentLogin[login]':login,
-        'FormStudentLogin[password]':password,
-        '_csrf-frontend':hidden_input
-    }
-
-    req_login = requests.post('https://student.andmiedu.uz/dashboard/login',data=payload, cookies=dict(req.cookies), allow_redirects=False)
-    output = dict(req_login.cookies)
-    if '_frontendUser' in output:
-        return output
-    else:
-        return False
-# shu asosiy cookielarni olib beruvchi funksiya bo'lib ishlashligi kerak
-def main_cookies(login, password):
-    cookies = db.get_cookies(login)
-    if cookies != False and cookies != 'expired':
-        return cookies
-    if password == '':
-        return False
-    auth_cookies = auth(login, password)
-    if cookies == False:
-        if auth_cookies == False:
-            return False
-        else:
-            db.insert_cookies(login, json.dumps(auth_cookies))
-            return auth_cookies
-    elif cookies == 'expired':
-        if auth_cookies == False:
-            return False
-        else:
-            db.update_cookies(login, json.dumps(auth_cookies))
-            return auth_cookies
-
-# nblarni olib beruvchi funksiya, dictionary return qiladi
-# aynan kimi davomati kerakligini login orqali aniqlashtirib oladi
-def get_davomat(login):
-    m_cookies = main_cookies(login, '')
-    if m_cookies:
-        davomat_html = requests.get('https://student.andmiedu.uz/education/attendance', cookies=m_cookies).text
-        soup = BeautifulSoup(davomat_html,'lxml')
-        tbody = soup.find('tbody')
-        trs = tbody.find_all('tr')
-        db = []
-        for tr in trs:
-            tds = tr.find_all('td')
-            data = {
-                'semestr':tds[1].text,
-                'dars_sanasi':tds[2].text,
-                'fanlar':tds[3].text,
-                'mashg\'ulot':tds[4].text,
-                'sababli':tds[5].text,
-                'soatlar':tds[6].text,
-                'xodim':tds[7].text
+            payload = {
+                'FormStudentLogin[login]':login,
+                'FormStudentLogin[password]':password,
+                '_csrf-frontend':hidden_input
             }
-            db.append(data)
-        return db
-    else:
-        return 'not_logged_in'
+
+            req_login = requests.post(URL + '/dashboard/login',data=payload, cookies=dict(req.cookies), allow_redirects=False)
+            output = dict(req_login.cookies)
+            if '_frontendUser' in output:
+                self.cookies = output
+                return output
+            else:
+                return False
+        
+        cookies = db.get_cookies(login)
+        if cookies == False:
+            auth_cookies = auth(login, password)
+            if auth_cookies != False:
+                db.insert_cookies(login, json.dumps(auth_cookies))
+        elif cookies == 'expired':
+            auth_cookies = auth(login, password)
+            if auth_cookies != False:
+                db.update_cookies(login, json.dumps(auth_cookies))
+
+    # nblarni olib beruvchi funksiya, dictionary return qiladi
+    # aynan kimi davomati kerakligini login orqali aniqlashtirib oladi
+    # Bu funksiya hozircha faqat bir dona o'qilyotgan semestrni oladi, kerak bo'lsa boshqalarini ham olsak bo'ladi keyinchalik
+    def get_davomat(self):
+        m_cookies = self.cookies
+        if m_cookies != False:
+            try:
+                davomat_html = requests.get(URL + '/education/attendance', cookies=m_cookies).text
+                soup = BeautifulSoup(davomat_html,'lxml')
+                tbody = soup.find('tbody')
+                trs = tbody.find_all('tr')
+                db = []
+                for tr in trs:
+                    tds = tr.find_all('td')
+                    data = {
+                        'semestr':tds[1].text,
+                        'dars_sanasi':tds[2].text,
+                        'fanlar':tds[3].text,
+                        'mashg\'ulot':tds[4].text,
+                        'sababli':tds[5].text,
+                        'soatlar':tds[6].text,
+                        'xodim':tds[7].text
+                    }
+                    db.append(data)
+                return db
+            except:
+                return False
+        else:
+            return 'not_logged_in'
+
+    # demak bu funksiya oraqali talabani zachotkasidagi baholarni olish mumkin
+    def get_uzlashtirish(self):
+        m_cookies = self.cookies
+        if m_cookies != False:
+            uzlashtirish_html = requests.get(URL + '/education/performance', cookies=m_cookies).text
+            soup = BeautifulSoup(uzlashtirish_html, 'lxml')
+            titles = soup.find_all('h3', class_='box-title')
+            tbodies = soup.find_all('tbody')
+            db = {}
+            db_t = []
+            for title in titles:
+                db_t.append(title.text)
+            db['titles'] = db_t
+            index = 0
+            for tbody in tbodies:
+                trs = tbody.find_all('tr')
+                db_tbody = []
+                for tr in trs:
+                    tds = tr.find_all('td')
+                    data = {
+                        'fan':tds[1].text.replace('\n','').strip(),
+                        'jn':tds[2].text.replace('\n','').strip(),
+                        'on':tds[3].text.replace('\n','').strip(),
+                        'yn':tds[4].text.replace('\n','').strip()
+                    }
+                    db_tbody.append(data)
+                db[index] = db_tbody 
+                index += 1
+            #pprint.pprint(db)
+        else:
+            return 'not_logged_in'
 
 
-#d = auth('303201100507','7xGjBZ2Fa24eHKW')
-main_cookies('303201100507','7xGjBZ2Fa24eHKW')
-d = get_davomat('303201100507')
-print(d)
+
+
+s = SCRAPER('303201100507','')
+# d = s.get_davomat()
+s.get_uzlashtirish()
